39cf2d18331246bfa9ee3526b059ba0829ad45a1
Valdaria <Soulitor505@gmail.com>
2022-12-10 23:08:45 +0100

    Day 3 : part two

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -4,7 +4,7 @@ pub mod day3 {
 
     #[warn(dead_code)]
     #[cfg(test)]
-    pub fn solve(input: &str) {
+    pub fn solve_part_one(input: &str) {
 
 
         #[cfg(windows)]
@@ -49,6 +49,37 @@ pub mod day3 {
 
     }
 
+    #[warn(dead_code)]
+    #[cfg(test)]
+    pub fn solve_part_two(input: &str) {
+
+
+        #[cfg(windows)]
+        let LINE_ENDING: &'static str = "\r\n";
+        #[cfg(not(windows))]
+        let LINE_ENDING: &'static str = "\n";
+
+        let chars = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+        let format_str = format!("{}", LINE_ENDING);
+        let mut rucksacks_iter = input.split(format_str.as_str()).into_iter();
+        let rucksack_size = rucksacks_iter.clone().count();
+
+        let mut sum_badges = 0;
+
+        for _ in (0..rucksack_size).step_by(3) {
+            let first_rucksack = rucksacks_iter.next().unwrap();
+            let second_rucksack = rucksacks_iter.next().unwrap();
+            let third_rucksack = rucksacks_iter.next().unwrap();
+            let same_letter = get_same_letter_in_three(first_rucksack, second_rucksack, third_rucksack);
+            let p = chars.chars().position(|c| c == same_letter).unwrap() as i32;
+            sum_badges += p;
+        }
+
+        println!("The Sum of priorities of badges is: {}", sum_badges);
+    }
+
     #[cfg(test)]
     fn get_same_letter(a : &str, b : &str) -> char {
         for letter_a in a.chars().into_iter(){
@@ -59,15 +90,32 @@ pub mod day3 {
         return '_';
     }
 
+    #[cfg(test)]
+    fn get_same_letter_in_three(a : &str, b : &str, c : &str) -> char {
+        for letter_a in a.chars().into_iter(){
+            for letter_b in b.chars().into_iter() {
+                if letter_a == letter_b {
+                    for letter_c in c.chars().into_iter() {
+                        if letter_a == letter_c {
+                            return letter_a
+                        }
+                    }
+                };
+            }
+        }
+        return '_';
+    }
+
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{utils, day3::day3::solve};
+    use crate::{utils, day3::day3::{solve_part_one, solve_part_two}};
     #[test]
     fn test_day2() {
         let input = utils::get_input_of_the_day("day3\\input.txt");
-        solve(input.as_str());
+        solve_part_one(input.as_str());
+        solve_part_two(input.as_str());
     }
 
 }
